bf29f6582ffa98cb3b6bef78fc6d3162a54f74ba
SATHISH P <141696697+sathish46-lab@users.noreply.github.com>
2024-04-12 11:34:05 +0530

    Update firebase.js
    Firebase configuration remove

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,11 +1,7 @@
-let firebaseConfig = {
-    apiKey: "AIzaSyB_HRNxCH2uOKsHfaXkolyzmnrykq_CWMw",
-    authDomain: "tom-website-1ba23.firebaseapp.com",
-    projectId: "tom-website-1ba23",
-    storageBucket: "tom-website-1ba23.appspot.com",
-    messagingSenderId: "555153773493",
-    appId: "1:555153773493:web:65de83d292391aafdd3ffc"
-  };
+let firebaseConfig = 
+{
+    // PUT Your FireBase Confit file here
+};
 
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
